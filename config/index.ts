339040f7ac761f0e3e3552beff3a304d75b61c9b
Ákos Beke <akosbeke@gmail.com>
2021-10-23 10:01:52 +0200

    Added notification

--- config/index.ts
+++ config/index.ts
@@ -17,6 +17,7 @@ export const USERS: UserItemProps[] = [
   },
 ]
 export const NOTIFICATIONS = [
-  { id: 0, text: 'Új megjelenés, amire egy egész ország várt: Elk*rtuk' },
-  { id: 1, text: '"Mi magyarok tudjuk, hogy mit szeretünk nézni" - elindult a NERflix' },
+  { id: 0, text: 'Rekordokat döntöget az Elk*rtuk az IMDB-n' },
+  { id: 1, text: 'Új megjelenés, amire egy egész ország várt: Elk*rtuk' },
+  { id: 2, text: '"Mi magyarok tudjuk, hogy mit szeretünk nézni" - elindult a NERflix' },
 ]
