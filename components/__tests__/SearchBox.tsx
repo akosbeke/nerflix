76eea60275d68a89db0de3ad104c6c5e32c1b544
Ákos Beke <akosbeke@gmail.com>
2021-10-23 09:31:16 +0200

    Updated search test

--- components/__tests__/SearchBox.tsx
+++ components/__tests__/SearchBox.tsx
@@ -2,11 +2,13 @@ import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderHook } from '@testing-library/react-hooks'
 import { useRouter } from 'next/router'
+import { event as mockEvent } from '../../lib/ga'
 
 import { render } from '../../test/providers'
 import SearchBox from '../SearchBox'
 
 jest.mock('next/dist/client/router', () => require('next-router-mock'))
+jest.mock('../../lib/ga')
 
 test('SearchBox should output a search icon and expand if clicked', () => {
   render(<SearchBox />)
@@ -23,5 +25,6 @@ test('SearchBox should output a search icon and expand if clicked', () => {
 
   const { result } = renderHook(() => useRouter())
 
+  expect(mockEvent).toHaveBeenCalledTimes(1)
   expect(result.current.asPath).toBe('/search/test')
 })
